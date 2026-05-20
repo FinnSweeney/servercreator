26d2ad2716132469df5b9d00eec55e4ab8e2fd35
BuildTools <unconfigured@null.spigotmc.org>
2026-05-20 10:24:10 -0700

    adding plugin finder + added plugin folder navigation

--- main.py
+++ main.py
@@ -14,13 +14,23 @@ def fileCreator():
 if __name__ == '__main__':
     jarFile = scrapeServerJar()
     shFileName = fileCreator()
+    splitString = jarFile.split("/")
+    jarName = splitString[len(splitString) - 1]
     with open(shFileName,'a') as f:
         f.write("/*  #!/bin/bash \n")
         f.write("# get server jar \n")
         f.write("wget " + jarFile + "\n")
+        f.write("java -jar " + jarName + "\n")
+    #navigate to plugins folder
+        f.write("cd plugins \n")
+
+    #start downloading plugins
     plugins = addPlugins()
     for i in range(len(plugins) - 1):
         print(scrapePlugin(int(plugins[i])))
+        with open(shFileName, 'a') as f:
+            f.write("wget " + scrapePlugin(int(plugins[i])) + "\n")
+
 
 
 
